5c6596fcc553ae40579b9643fa1b3fd95267ccbe
Bhavya Gor <bhavya.gor9999@gmail.com>
2024-05-09 16:27:29 +0530

    pages setup complete

---
+++ packages/nextjs/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import type { NextPage } from "next";
+
+const Profile: NextPage = () => {
+  return (
+    <>
+      <div className="flex items-center flex-col flex-grow pt-10"></div>
+    </>
+  );
+};
+
+export default Profile;
---
+++ packages/nextjs/app/users/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import type { NextPage } from "next";
+
+const User: NextPage = () => {
+  return (
+    <>
+      <div className="flex items-center flex-col flex-grow pt-10"></div>
+    </>
+  );
+};
+
+export default User;
